b1647e5dea6b70ac73351add54f845c3ebac66d6
Manitra Andriamitondra <manitra@manitra.net>
2015-07-22 10:29:01 +0700

    Initial quick and dirty commit

---
+++ src/PfxBruteForce.UI/MainForm.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.certificatePath = new System.Windows.Forms.TextBox();
+            this.go = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.minChar = new System.Windows.Forms.NumericUpDown();
+            this.maxChar = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.minChar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.maxChar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // certificatePath
+            // 
+            this.certificatePath.Location = new System.Drawing.Point(12, 12);
+            this.certificatePath.Name = "certificatePath";
+            this.certificatePath.Size = new System.Drawing.Size(361, 20);
+            this.certificatePath.TabIndex = 0;
+            this.certificatePath.Text = "C:\\Users\\m.andriamitondra\\Desktop\\certif.p12";
+            // 
+            // go
+            // 
+            this.go.Location = new System.Drawing.Point(392, 9);
+            this.go.Name = "go";
+            this.go.Size = new System.Drawing.Size(75, 23);
+            this.go.TabIndex = 1;
+            this.go.Text = "Go";
+            this.go.UseVisualStyleBackColor = true;
+            this.go.Click += new System.EventHandler(this.go_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 139);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(644, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(48, 17);
+            this.toolStripStatusLabel1.Text = "Waiting";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel2.Text = "toolStripStatusLabel2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(181, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Result:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(233, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "...";
+            // 
+            // minChar
+            // 
+            this.minChar.Location = new System.Drawing.Point(99, 40);
+            this.minChar.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.minChar.Name = "minChar";
+            this.minChar.Size = new System.Drawing.Size(72, 20);
+            this.minChar.TabIndex = 4;
+            this.minChar.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            // 
+            // maxChar
+            // 
+            this.maxChar.Location = new System.Drawing.Point(177, 40);
+            this.maxChar.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.maxChar.Name = "maxChar";
+            this.maxChar.Size = new System.Drawing.Size(72, 20);
+            this.maxChar.TabIndex = 4;
+            this.maxChar.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Password length";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 161);
+            this.Controls.Add(this.maxChar);
+            this.Controls.Add(this.minChar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.go);
+            this.Controls.Add(this.certificatePath);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.minChar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.maxChar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox certificatePath;
+        private System.Windows.Forms.Button go;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.NumericUpDown minChar;
+        private System.Windows.Forms.NumericUpDown maxChar;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ src/PfxBruteForce.UI/MainForm.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class MainForm : Form
+    {
+        private IList<char> options;
+        private Thread worker;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            options = new List<char>();
+            for (char i = 'a'; i <= 'z'; i++)
+                options.Add(i);
+            //for (char i = 'A'; i <= 'Z'; i++)
+            //    options.Add(i);
+            //for (char i = '0'; i <= '9'; i++)
+            //    options.Add(i);
+        }
+
+        private void go_Click(object sender, EventArgs e)
+        {
+            worker = new Thread(() =>
+                {
+
+                    long count = 0;
+                    var start = DateTime.UtcNow;
+
+                    Parallel.ForEach(
+                        EnumerateOptions(options, (int)minChar.Value, (int)maxChar.Value),
+                        new ParallelOptions { MaxDegreeOfParallelism = 8 },
+                        password =>
+                        {
+                            if (count % 10 == 0)
+                            {
+                                Invoke(new Action(() =>
+                                {
+                                    toolStripStatusLabel1.Text = "Trying " + password;
+                                    var elapsed = DateTime.UtcNow - start;
+                                    toolStripStatusLabel2.Text = string.Format(
+                                        "{0:n} tests per seconds since {1:t}",
+                                        count / elapsed.TotalSeconds, elapsed
+                                    );
+                                }));
+                                Application.DoEvents();
+                            }
+
+                            if (TryPassword(password))
+                            {
+                                BeginInvoke(new Action(() =>
+                                {
+                                    MessageBox.Show("Found : " + password);
+                                    label2.Text = password;
+
+                                }));
+                            }
+
+                            count++;
+                        }
+                    );
+                });
+            worker.Start();
+        }
+
+        private IEnumerable<string> EnumerateOptions(IList<char> options, int minLength, int maxLength)
+        {
+            var number = new List<int>(maxLength);
+            for (int i = 0; i < minLength; i++)
+                number.Add(0);
+
+            while (number.Count <= maxLength)
+            {
+                yield return Stringify(number, options);
+
+                bool overflowed = true;
+                for (int numberPart = number.Count - 1; numberPart >= 0 && overflowed; numberPart--)
+                {
+                    number[numberPart]++;
+                    if (number[numberPart] >= options.Count)
+                    {
+                        number[numberPart] = number[numberPart] % options.Count;
+                    }
+                    else
+                    {
+                        overflowed = false;
+                    }
+                }
+                if (overflowed)
+                    number.Insert(0, 0);
+            }
+        }
+
+        private string Stringify(IList<int> number, IList<char> options)
+        {
+            return string.Join("", number.Select(n => options[n]));
+        }
+
+        private bool TryPassword(string password)
+        {
+            try
+            {
+                var collection = new X509Certificate2Collection();
+                collection.Import(certificatePath.Text, password, X509KeyStorageFlags.PersistKeySet);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
