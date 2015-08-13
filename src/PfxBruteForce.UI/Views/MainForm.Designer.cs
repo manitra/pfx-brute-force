07feaeadc2d99689fb59f811e08a020c0b76d513
Manitra Andriamitondra <manitra@manitra.net>
2015-08-13 14:42:11 +0700

    Move the form to the Views namespace

--- src/PfxBruteForce.UI/Views/MainForm.Designer.cs
+++ src/PfxBruteForce.UI/Views/MainForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WindowsFormsApplication1
+﻿namespace PfxBruteForce.UI.Views
 {
     partial class MainForm
     {
--- src/PfxBruteForce.UI/Views/MainForm.cs
+++ src/PfxBruteForce.UI/Views/MainForm.cs
@@ -3,18 +3,14 @@ using System.IO.Compression;
 using System.Linq;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Security.Cryptography.X509Certificates;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WindowsFormsApplication1
+namespace PfxBruteForce.UI.Views
 {
     public partial class MainForm : Form
     {
