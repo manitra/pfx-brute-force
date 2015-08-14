a883e21b3898428013f98642fe3b6fbcdb0251a4
Manitra Andriamitondra <manitra@manitra.net>
2015-08-14 17:35:14 +0700

    Add Unit Test project and realize the issue for testing MainController

---
+++ src/PfxBruteForce.UI.Tests/Controllers/MainControllerTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PfxBruteForce.UI.Controllers;
+
+namespace PfxBruteForce.UI.Tests.Controllers
+{
+    [TestFixture]
+    public class MainControllerTest
+    {
+        [Test]
+        public void Constructing_DoesNotThrow()
+        {
+            var target = new MainController();
+        }
+
+        [Test]
+        public void Init_IsNotRunningByDefault()
+        {
+            var target = new MainController();
+
+            var data = target.Init();
+
+            Assert.IsFalse(data.Running);
+        }
+
+        [Test]
+        public async void Start_FindTotoPassword()
+        {
+            var target = new MainController();
+
+            //target.Start(parameters);
+
+            // to test this, i have to
+            // - give a real certificate file path
+            // - give a real url to an online gzipped compressed text file
+            // - i need internet connection to access that file
+            // - it will take ages for the method to actually find the password
+            Assert.Inconclusive();
+        }
+    }
+}
